41a9f80ecfc6bfde11020fd4a4f70aad343decc1
Moritz Schönherr <moritz.schoenherr@posteo.net>
2019-01-25 17:34:47 +0100

    org: licensing disclaimer for world

--- src/ironsworn_companion/world.cljs
+++ src/ironsworn_companion/world.cljs
@@ -1,5 +1,9 @@
 (ns ironsworn-companion.world)
 
+;; This work is based on Ironsworn (found at www.ironswornrpg.com), created by Shawn Tomkin,
+;; and licensed for our use under the Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International license
+;; (creativecommons.org/licenses/by-nc-sa/4.0/).
+
 (def world-options
   [{:name "The old world"
     :selected-val "The savage clans ..."
